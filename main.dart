c662fdb4f8ec6cad0e1476601e40b6b41ef7a96f
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2024-09-03 08:25:47 -0300

    aula 03/09/2024

--- main.dart
+++ main.dart
@@ -1,45 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.red,
-              title: const Text('Login'),
-            ),
-            drawer: const Drawer(backgroundColor: Colors.black),
-            body: Align(
-              alignment: Alignment.center,
-              child: SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: Column(children: [
-                    const TextField(
-                      decoration: InputDecoration(labelText: 'E-mail'),
-                      style: TextStyle(color: Colors.red, fontSize: 20),
-                    ),
-                    const TextField(
-                      decoration: InputDecoration(labelText: 'Senha'),
-                      style: TextStyle(color: Colors.red, fontSize: 20),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {}, child: const Text('Entrar')),
-                  ])),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person), label: 'Minha conta'),
-              ],
-            )));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Column(children: [
+              SizedBox(
+                width: 0.8 * screenwidth,
+                child: Image.asset('assets/images/cachorro.jpg'),
+              )
+            ])),
+      ),
+    );
   }
 }
